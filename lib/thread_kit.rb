c81c4e67576a8230efe56c3399522d537ac10d80
Josh Lauer <josh.lauer75@gmail.com>
2015-02-07 22:20:32 -0500

    added thread local store and accessors

--- lib/thread_kit.rb
+++ lib/thread_kit.rb
@@ -1,6 +1,7 @@
 require "thread_kit/version"
+require "thread_kit/thread_local/store"
+require "thread_kit/thread_local/accessors"
 require "thread_kit/pool"
 
 module ThreadKit
-  # Your code goes here...
 end
