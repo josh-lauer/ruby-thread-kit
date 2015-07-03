4eafebe1c512d5dfd7b789a014284e1724720e0a
Josh Lauer <philip.lauer@jdpa.com>
2015-07-03 11:10:53 -0400

    added cattr accessors and accessor tests

--- lib/thread_kit/thread_local/accessors.rb
+++ lib/thread_kit/thread_local/accessors.rb
@@ -1,37 +1,57 @@
 module ThreadKit
   module ThreadLocal
     module Accessors
-      def self.included(base)
-        base.extend(ClassMethods)
+      # thread local attr_accessor
+      def tl_attr_accessor(*attrs, default: nil)
+        tl_attr_reader(*attrs, default: default)
+        tl_attr_writer(*attrs)
       end
 
-      def self.extended(base)
-        fail "#{self.name} is not meant to be extended. use include instead"
-      end
-
-      module ClassMethods
-        # thread local attr_accessor
-        def tl_attr_accessor(*method_names, default: nil)
-          tl_attr_reader(*method_names, default: default)
-          tl_attr_writer(*method_names)
-        end
-
-        # thread local attr_reader
-        def tl_attr_reader(*attrs, default: nil)
-          attrs.each do |attribute|
-            define_method(attribute) do
-              Store.get(self, attribute, default: default)
-            end
+      # thread local attr_reader
+      def tl_attr_reader(*attrs, default: nil)
+        attrs.each do |attribute|
+          define_method(attribute) do
+            Store.get(self, attribute, default: default)
           end
         end
+      end
 
-        # thread local attr_writer
-        def tl_attr_writer(*attrs)
-          attrs.each do |attribute|
-            method_name = "#{attribute}=".to_sym
-            define_method(method_name) do |new_value|
-              Store.set(self, attribute, new_value)
-            end
+      # thread local attr_writer
+      def tl_attr_writer(*attrs)
+        attrs.each do |attribute|
+          method_name = "#{attribute}=".to_sym
+          define_method(method_name) do |new_value|
+            Store.set(self, attribute, new_value)
+          end
+        end
+      end
+
+      def tl_cattr_accessor(*attrs, default: nil)
+        tl_cattr_reader(*attrs, default: default)
+        tl_cattr_writer(*attrs)
+      end
+
+      def tl_cattr_reader(*attrs, default: nil)
+        attrs.each do |attribute|
+          define_method(attribute) do
+            Store.get(self.class, attribute, default: default)
+          end
+
+          define_singleton_method(attribute) do
+            Store.get(self, attribute, default: default)
+          end
+        end
+      end
+
+      def tl_cattr_writer(*attrs)
+        attrs.each do |attribute|
+          method_name = "#{attribute}=".to_sym
+          define_method(method_name) do |new_value|
+            Store.set(self.class, attribute, new_value)
+          end
+
+          define_singleton_method(method_name) do |new_value|
+            Store.set(self, attribute, new_value)
           end
         end
       end
--- spec/thread_kit/thread_local/accessors_spec.rb
+++ spec/thread_kit/thread_local/accessors_spec.rb
@@ -1,2 +1,91 @@
-describe "ThreadKit::ThreadLocal::Accessors" do
+describe ThreadKit::ThreadLocal::Accessors do
+
+  let(:klass) do
+    Class.new(Object) do
+      extend ThreadKit::ThreadLocal::Accessors
+      tl_attr_accessor :tl_aa
+      tl_attr_reader :tl_ar
+      tl_attr_writer :tl_aw
+      tl_cattr_accessor :tl_ca
+      tl_cattr_reader :tl_cr
+      tl_cattr_writer :tl_cw
+    end
+  end
+
+  let(:klass_instance) { klass.new }
+
+  context "#tl_attr_accessor" do
+    it "responds to both reader and writer methods" do
+      expect(klass_instance).to respond_to(:tl_aa)
+      expect(klass_instance).to respond_to(:tl_aa=)
+    end
+  end
+
+  context "#tl_attr_reader" do
+    it "responds to the reader method" do
+      expect(klass_instance).to respond_to(:tl_ar)
+    end
+
+    it "does not respond to the writer method" do
+      expect(klass_instance).to_not respond_to(:tl_ar=)
+    end
+  end
+
+  context "#tl_attr_writer" do
+    it "responds to the writer method" do
+      expect(klass_instance).to respond_to(:tl_aw=)
+    end
+
+    it "does not respond to the reader method" do
+      expect(klass_instance).to_not respond_to(:tl_aw)
+    end
+  end
+
+  context "#tl_cattr_accessor" do
+    it "responds to reader and writer methods at the class level" do
+      expect(klass).to respond_to(:tl_ca)
+      expect(klass).to respond_to(:tl_ca=)
+    end
+
+    it "responds to reader and writer methods at the instance level" do
+      expect(klass_instance).to respond_to(:tl_ca)
+      expect(klass_instance).to respond_to(:tl_ca=)
+    end
+  end
+
+  context "#tl_cattr_reader" do
+    it "responds to the reader method at the class level" do
+      expect(klass).to respond_to(:tl_cr)
+    end
+
+    it "does not respond to the writer method at the class level" do
+      expect(klass).to_not respond_to(:tl_cr=)
+    end
+
+    it "responds to the reader method at the instance level" do
+      expect(klass_instance).to respond_to(:tl_cr)
+    end
+
+    it "does not respond to the writer method at the instance level" do
+      expect(klass_instance).to_not respond_to(:tl_cr=)
+    end
+  end
+
+  context "#tl_cattr_writer" do
+    it "responds to the writer method at the class level" do
+      expect(klass).to respond_to(:tl_cw=)
+    end
+
+    it "does not respond to the reader method at the class level" do
+      expect(klass).to_not respond_to(:tl_cw)
+    end
+
+    it "responds to the writer method at the instance level" do
+      expect(klass_instance).to respond_to(:tl_cw=)
+    end
+
+    it "does not respond to the reader method at the instance level" do
+      expect(klass_instance).to_not respond_to(:tl_cw)
+    end
+  end
 end
