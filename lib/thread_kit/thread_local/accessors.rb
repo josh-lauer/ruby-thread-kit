a6f526cc36cd264be7c9d9d4266027baf07eba76
Josh Lauer <philip.lauer@jdpa.com>
2015-07-04 11:36:42 -0400

    accessors now blindly pass options to Store

--- lib/thread_kit/thread_local/accessors.rb
+++ lib/thread_kit/thread_local/accessors.rb
@@ -2,56 +2,59 @@ module ThreadKit
   module ThreadLocal
     module Accessors
       # thread local attr_accessor
-      def tl_attr_accessor(*attrs, default: nil)
-        tl_attr_reader(*attrs, default: default)
-        tl_attr_writer(*attrs)
+      def tl_attr_accessor(*attrs, **options)
+        tl_attr_reader(*attrs, **options)
+        tl_attr_writer(*attrs, **options)
       end
 
       # thread local attr_reader
-      def tl_attr_reader(*attrs, default: nil)
+      def tl_attr_reader(*attrs, **options)
         attrs.each do |attribute|
           define_method(attribute) do
-            Store.get(self, attribute, default: default)
+            Store.get(self, attribute, **options)
           end
         end
       end
 
       # thread local attr_writer
-      def tl_attr_writer(*attrs)
+      def tl_attr_writer(*attrs, **options)
         attrs.each do |attribute|
           method_name = "#{attribute}=".to_sym
           define_method(method_name) do |new_value|
-            Store.set(self, attribute, new_value)
+            Store.set(self, attribute, new_value, **options)
           end
         end
       end
 
-      def tl_cattr_accessor(*attrs, default: nil)
-        tl_cattr_reader(*attrs, default: default)
-        tl_cattr_writer(*attrs)
+      # thread local cattr_accesor
+      def tl_cattr_accessor(*attrs, **options)
+        tl_cattr_reader(*attrs, **options)
+        tl_cattr_writer(*attrs, **options)
       end
 
-      def tl_cattr_reader(*attrs, default: nil)
+      # thread local cattr_reader
+      def tl_cattr_reader(*attrs, **options)
         attrs.each do |attribute|
           define_method(attribute) do
-            Store.get(self.class, attribute, default: default)
+            Store.get(self.class, attribute, **options)
           end
 
           define_singleton_method(attribute) do
-            Store.get(self, attribute, default: default)
+            Store.get(self, attribute, **options)
           end
         end
       end
 
-      def tl_cattr_writer(*attrs)
+      # thread local cattr_writer
+      def tl_cattr_writer(*attrs, **options)
         attrs.each do |attribute|
           method_name = "#{attribute}=".to_sym
           define_method(method_name) do |new_value|
-            Store.set(self.class, attribute, new_value)
+            Store.set(self.class, attribute, new_value, **options)
           end
 
           define_singleton_method(method_name) do |new_value|
-            Store.set(self, attribute, new_value)
+            Store.set(self, attribute, new_value, **options)
           end
         end
       end
