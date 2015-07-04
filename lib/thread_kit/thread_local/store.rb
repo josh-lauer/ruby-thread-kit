35036124ec6f5d4c43c066f65014cb889715c13a
Josh Lauer <philip.lauer@jdpa.com>
2015-07-04 11:35:46 -0400

    refactored Store

--- lib/thread_kit/thread_local/store.rb
+++ lib/thread_kit/thread_local/store.rb
@@ -1,3 +1,6 @@
+require 'singleton'
+require 'forwardable'
+
 module ThreadKit
   module ThreadLocal
     class Store
@@ -5,63 +8,57 @@ module ThreadKit
 
       class << self
         extend Forwardable
-        def_delegators :instance, :set, :get, :key?
+        def_delegators :instance, :set, :get
       end
 
       # fetch the value for the given key in the current thread relative to the given object
-      def set(obj, key, val)
-        object_store(obj)[key.to_s] = val
+      def set(obj, key, val, **options)
+        register_finalizer(obj)
+        object_store(obj)[key.to_sym] = val
       end
 
-      # fetch the value for the given key in the current thread relative to the given object
-      def get(obj, key)
-        object_store(obj)[key.to_s]
+      # fetch the value for the given :key associated with :obj in Thread.current
+      # relative to the given :obj
+      def get(obj, key, **options)
+        object_store(obj)[key.to_sym]
       end
 
-      # is the given key set on the given object?
-      def key?(obj, key)
-        object_store(obj).key?(key)
+      # check if there is a value stored with :key relative to :obj
+      def object_has_key?(obj, key)
+        object_initialized?(obj) && object_store(obj).key?(key.to_sym)
+      end
+
+      # check if there are any values stored on the given object in the current thread
+      def object_initialized?(obj)
+        thread_store.key?(obj.__id__)
       end
 
       private
 
+      # define a finalizer for the given object unless one has already been defined for
+      # this object in the current thread
+      def register_finalizer(obj)
+        ObjectSpace.define_finalizer(obj, &method(:flush_object)) unless object_initialized?(obj)
+      end
+
+      # delete storage for the specified obj_id on the current thread
+      def flush_object(obj_id)
+        thread_store.delete(obj_id)
+      end
+
+      # returns the hash of values stored for :obj
       def object_store(obj)
         thread_store[obj.__id__]
       end
 
+      # returns a hash
       def thread_store
         Thread.current[root_key] ||= Hash.new { |h,k| h[k] = {} }
       end
 
       def root_key
-        :__thread_local_ivar_store__
+        :__threadkit_local_store__
       end
-
-      ############################
-      #### garbage collection ####
-      ############################
-
-      # def define_object_finalizer(obj)
-      #   # delete thread locals for this object from all threads when it is deconstructed
-      #   ObjectSpace.define_finalizer(obj) do |obj_id|
-      #     finalize_object!(obj_id)
-      #   end
-      # end
-
-      # def finalize_object!(obj_id)
-      #   store.each_pair { |_, objects| objects.delete(obj_id) }
-      # end
-
-      # def define_thread_finalizer(thread)
-      #   ObjectSpace.define_finalizer(thread) do |obj_id|
-      #     finalize_thread!(obj_id)
-      #   end
-      # end
-
-      # # delete all thread locals for this thread when it is deconstructed
-      # def finalize_thread!(obj_id)
-      #   store.delete(obj_id)
-      # end
     end
   end
 end
