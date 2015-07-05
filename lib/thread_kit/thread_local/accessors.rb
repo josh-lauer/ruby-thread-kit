7a2666620ad27d6a5fcbb1ae0cde400d21b6dbce
Josh Lauer <philip.lauer@jdpa.com>
2015-07-05 00:13:55 -0400

    added visibility flag for private/protected accessors

--- lib/thread_kit/thread_local/accessors.rb
+++ lib/thread_kit/thread_local/accessors.rb
@@ -9,20 +9,34 @@ module ThreadKit
 
       # thread local attr_reader
       def tl_attr_reader(*attrs, **options)
+        options = Hash[options.map{|(k,v)| [k.to_sym,v]}]
+        visibility = options.delete(:visibility)
+        if visibility && ![:public, :private, :protected].include?(visibility)
+          raise "visibility must be :public, :private, or :protected"
+        end
         attrs.each do |attribute|
           define_method(attribute) do
             Store.get(self, attribute, **options)
           end
+
+          send(visibility, attribute) if visibility
         end
       end
 
       # thread local attr_writer
       def tl_attr_writer(*attrs, **options)
+        options = Hash[options.map{|(k,v)| [k.to_sym,v]}]
+        visibility = options.delete(:visibility)
+        if visibility && ![:public, :private, :protected].include?(visibility)
+          raise "visibility must be :public, :private, or :protected"
+        end
         attrs.each do |attribute|
           method_name = "#{attribute}=".to_sym
           define_method(method_name) do |new_value|
             Store.set(self, attribute, new_value, **options)
           end
+
+          send(visibility, method_name) if visibility
         end
       end
 
@@ -34,6 +48,12 @@ module ThreadKit
 
       # thread local cattr_reader
       def tl_cattr_reader(*attrs, **options)
+        options = Hash[options.map{|(k,v)| [k.to_sym,v]}]
+        visibility = options.delete(:visibility)
+        if visibility && ![:public, :private, :protected].include?(visibility)
+          raise "visibility must be :public, :private, or :protected"
+        end
+        options = Hash[options.map{|(k,v)| [k.to_sym,v]}]
         attrs.each do |attribute|
           define_method(attribute) do
             Store.get(self.class, attribute, **options)
@@ -42,11 +62,22 @@ module ThreadKit
           define_singleton_method(attribute) do
             Store.get(self, attribute, **options)
           end
+
+          if visibility
+            send(visibility, attribute)
+            (class << self; self; end).instance_eval { send(visibility, attribute) }
+          end
         end
       end
 
       # thread local cattr_writer
       def tl_cattr_writer(*attrs, **options)
+        options = Hash[options.map{|(k,v)| [k.to_sym,v]}]
+        visibility = options.delete(:visibility)
+        if visibility && ![:public, :private, :protected].include?(visibility)
+          raise "visibility must be :public, :private, or :protected"
+        end
+        options = Hash[options.map{|(k,v)| [k.to_sym,v]}]
         attrs.each do |attribute|
           method_name = "#{attribute}=".to_sym
           define_method(method_name) do |new_value|
@@ -55,6 +86,11 @@ module ThreadKit
 
           define_singleton_method(method_name) do |new_value|
             Store.set(self, attribute, new_value, **options)
+          end
+
+          if visibility
+            send(visibility, method_name)
+            (class << self; self; end).instance_eval { send(visibility, method_name) }
           end
         end
       end
